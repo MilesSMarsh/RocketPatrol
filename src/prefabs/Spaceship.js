21ea0e23f61feaca0116d9ea5fe5aa29cc083c17
Miles Marsh <msmarsh@ucsc.edu>
2023-04-17 13:41:00 -0700

    collision detection

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,24 @@
+// Spaceship prefab
+class Spaceship extends Phaser.GameObjects.Sprite{
+    constructor(scene, x, y, texture, frame, pointValue){
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        this.points = pointValue;
+        this.moveSpeed = 3;
+    }
+
+    update(){
+        //move left
+        this.x -= this.moveSpeed;
+
+        //wrap around from left to right
+        if(this.x <= 0 - this.width){
+            this.x = game.config.width;
+        }
+    }
+
+
+
+
+
+}
